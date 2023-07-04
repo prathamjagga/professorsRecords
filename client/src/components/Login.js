23f22fde001562f5980f5fabc607e9d119c8ec73
Pratham Jagga <pratham.jagga@cloudeq.com>
2023-07-04 17:19:05 +0530

    started UI Improvements ;)

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./styles/login.module.css";
+import Logo from "../assets/logo.png";
+import IMSLogo from "../assets/ims-logo.png";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -43,29 +45,72 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div>
-          <label>Email:</label>
+    <div
+      style={{
+        display: "flex",
+        height: "100vh",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+    >
+      <img src={Logo} className="w-3/12 mb-8" />
+      <img src={IMSLogo} className="w-1/12 mb-12" />
+      <h2 className="text-lg block mb-8 text-sm font-medium text-black-900 dark:text-black">
+        Login to ProfessorIMS
+      </h2>
+
+      <form className="mb-6">
+        <div className="">
+          <label
+            for="phone"
+            className="block mb-2 text-sm font-medium text-black-900 dark:text-black"
+          >
+            Enter Phone Number
+          </label>
+        </div>
+        <div className="mb-6" style={{ display: "flex", flexDirection: "row" }}>
           <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            readOnly
+            type="text"
+            id="otp"
+            className=" text-center mr-2 w-12 bg-white-50 border border-white-300 text-black-900 text-sm rounded-lg focus:ring-white-500 focus:border-white-500 block w-full p-2.5 dark:bg-white-700 dark:border-white-600 dark:placeholder-white-400 dark:text-black dark:focus:ring-white-500 dark:focus:border-white-500"
+            value="+91"
+            required
+          />
+          <input
+            type="text"
+            id="otp"
+            className="bg-white-50 border border-white-300 text-black-900 text-sm rounded-lg focus:ring-white-500 focus:border-white-500 block w-full p-2.5 dark:bg-white-700 dark:border-white-600 dark:placeholder-white-400 dark:text-black dark:focus:ring-white-500 dark:focus:border-white-500"
+            placeholder="9782161905"
             required
           />
         </div>
-        <div>
-          <label>Password:</label>
+        <div className="mb-6">
+          <label
+            for="otp"
+            className="block mb-2 text-sm font-medium text-black-900 dark:text-black"
+          >
+            Enter OTP
+          </label>
           <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            type="text"
+            id="otp"
+            className="bg-white-50 border border-white-300 text-black-900 text-sm rounded-lg focus:ring-white-500 focus:border-white-500 block w-full p-2.5 dark:bg-white-700 dark:border-white-600 dark:placeholder-white-400 dark:text-black dark:focus:ring-white-500 dark:focus:border-white-500"
             required
           />
         </div>
-        <button className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" type="submit">Login</button>
+        <div className="flex justify-center items-center">
+          {" "}
+          <button
+            type="submit"
+            className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+          >
+            Submit
+          </button>
+        </div>
       </form>
+
       <p>
         Don't have an account? <Link to="/register">Register</Link>
       </p>
